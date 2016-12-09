c710759c200431503d5ca0318795e129acf4a546
distraught1 <nickweddleton@gmail.com>
2016-12-08 19:19:31 -0600

    core

--- core.js
+++ core.js
@@ -4,6 +4,9 @@ $.ajax({
     $("body").prepend(data);
     setTimeout(function(e){
         $("#cover").hide();
+        var urlPieces = document.location.href.split("/");
+        var currentPage = urlPieces[urlPieces.length-1];
+        $("nav a[href='"+currentPage+"']").addClass("currentPage");
     }, 1);
 });
 
