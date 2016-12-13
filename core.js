ef242be109d1b059e7bba58090d6c0cea33b1476
distraught1 <nickweddleton@gmail.com>
2016-12-12 19:24:50 -0600

    Mobile Menu CSS

--- core.js
+++ core.js
@@ -1,3 +1,13 @@
+function scrollToElement(e){
+    var idToScroll = $(e.target).attr("href");
+    if(idToScroll != "" && idToScroll != "#"){
+        $('html, body').animate({
+            scrollTop: $(idToScroll).offset().top - $("header").height()
+        }, 500);                   
+    }
+    return false;
+}
+
 $.ajax({
     url: "BridgesHeader.html"
 }).success(function(data){
@@ -7,6 +17,22 @@ $.ajax({
         var urlPieces = document.location.href.split("/");
         var currentPage = urlPieces[urlPieces.length-1];
         $("nav a[href='"+currentPage+"']").addClass("currentPage");
+        $("#mobileMenuWrapper").click(function(e){
+            $("#modalCover").show();
+            $("nav").css("right", "-35vw");
+            $("nav").animate({
+                right: "0vw"
+            }, 500);
+        });
+        
+        $("#modalCover").click(function(e){
+            $("#modalCover").hide();
+            $("nav").animate({
+                right: "-35vw"
+            }, 500, function(){
+                $("nav").css("right", "");
+            });
+        });
     }, 1);
 });
 
@@ -15,3 +41,5 @@ $.ajax({
 }).success(function(data){
     $("body").append(data);
 });
+
+
